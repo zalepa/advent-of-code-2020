bb798439fb6d73da67bbcc1e8e953c8f1d2848e6
George Zalepa <george@purity.io>
2020-12-06 07:22:16 -0500

    Refactor

--- day05/challenge1.js
+++ day05/challenge1.js
@@ -1,49 +1,54 @@
 const helpers = require('../helpers');
 
+function binarySpaceParition(range, split) {
+  if (!split.match(/upper|lower/)) {
+    throw new Error('split parameter must be "lower" or "upper"');
+  }
+
+  let min = range[0];
+  let max = range[1];
+
+  const dist = max - min;
+  const midpoint = dist / 2;
+
+  if (split === 'lower') {
+    max = Math.floor(midpoint + min);
+  } else if (split === 'upper') {
+    min = Math.ceil(midpoint + min);
+  }
+
+  return [min, max]
+}
+
 function findRow(str) {
   const totalRows = 128;
   const totalCols = 8;
-  const dirs = str.split('');
   let row = 0;
   let seat = 0;
-
+  
   let currentRowMin = 0;
   let currentRowMax = totalRows - 1;
-
   let currentColMin = 0;
   let currentColMax = totalCols - 1;
 
+  const dirs = str.split('');
+
   dirs.forEach(dir => {
-    if (dir === 'F' || dir === 'B') {
+
+    let direction = 'lower';
+    if (dir.match(/B|R/)) direction = 'upper'
+
+    if (dir.match(/F|B/)) {
+      [currentRowMin, currentRowMax] = binarySpaceParition([currentRowMin, currentRowMax], direction)
+      if (currentRowMin === currentRowMax) row = currentRowMin;
+    }
       
-      let dist = currentRowMax - currentRowMin;
-      let midpoint = dist / 2;
-
-      if (dir === 'F') {
-        currentRowMax = Math.floor(midpoint + currentRowMin);
-      } else if (dir === 'B') {
-        currentRowMin = Math.ceil(midpoint + currentRowMin);
-      }
-
-      if (currentRowMin === currentRowMax) {
-        row = currentRowMin;
-      }
-    } else if (dir === 'L' || dir === 'R') {
-
-      let dist = currentColMax - currentColMin;
-      let midpoint = dist / 2;
-
-      if (dir === 'L') {
-        currentColMax = Math.floor(midpoint + currentColMin);
-      } else if (dir === 'R') {
-        currentColMin = Math.ceil(midpoint + currentColMin);
-      }
-
-      if (currentColMin === currentColMax) {
-        seat = currentColMin;
-      }
+    else if (dir.match(/R|L/)) {
+      [currentColMin, currentColMax] = binarySpaceParition([currentColMin, currentColMax], direction);
+      if (currentColMin === currentColMax) seat = currentColMin;
     }
   });
+
   return [row, seat];
 }
 
